0a768e37dc6e8ee30aa0de3c3fa99da3b7b2e1a5
Kyle Rumble <kylerumble@gmail.com>
2018-10-26 16:22:47 -0700

    add favicons

--- wp-content/themes/build-my-practice/header.php
+++ wp-content/themes/build-my-practice/header.php
@@ -16,11 +16,11 @@
 <link rel="profile" href="http://gmpg.org/xfn/11">
 <link rel="pingback" href="<?php bloginfo( 'pingback_url' ); ?>">
 <link rel="dns-prefetch" href="//fonts.googleapis.com">
-<link rel="apple-touch-icon" sizes="60x60" href="<?php echo THEME_FAVICONS;?>/apple-touch-icon.png">
+<link rel="apple-touch-icon" sizes="180x180" href="<?php echo THEME_FAVICONS;?>/apple-touch-icon.png">
 <link rel="icon" type="image/png" sizes="32x32" href="<?php echo THEME_FAVICONS;?>/favicon-32x32.png">
 <link rel="icon" type="image/png" sizes="16x16" href="<?php echo THEME_FAVICONS;?>/favicon-16x16.png">
 <link rel="manifest" href="<?php echo THEME_FAVICONS;?>/manifest.json">
-<link rel="mask-icon" href="<?php echo THEME_FAVICONS;?>/safari-pinned-tab.svg" color="#5bbad5">
+<link rel="mask-icon" href="<?php echo THEME_FAVICONS;?>/safari-pinned-tab.svg" color="#51B6C8">
 <meta name="theme-color" content="#ffffff">
 <?php wp_head(); ?>
 </head>
